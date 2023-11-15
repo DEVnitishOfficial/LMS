55f17ae624066e33811ab99d53592caadc081eb3
DEVnitishOfficial <nitish.naroun123@gmail.com>
2023-11-15 12:32:40 +0530

    started adding redux store and Slices

---
+++ client/src/Redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authSliceReducer from './Slices/AuthSlice.js'
+
+const store = configureStore({
+    reducer : {
+        auth : authSliceReducer
+    },
+    devTools : true
+})
+
+export default store
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,18 @@ import './index.css'
 // Liberary import
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 // Components import
 import App from './App.jsx'
+import store from './Redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}> 
   <BrowserRouter>
     <App />
     <Toaster />
   </BrowserRouter>
+  </Provider>
 )
